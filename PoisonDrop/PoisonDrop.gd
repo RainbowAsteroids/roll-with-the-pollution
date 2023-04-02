d31818c4d059bc8831b23aa3ae7885c9ec8c8929
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-04-02 06:50:40 -0400

    Added sprites for poison drops

--- PoisonDrop/PoisonDrop.gd
+++ PoisonDrop/PoisonDrop.gd
@@ -13,6 +13,16 @@ var radius: float:
         shape.radius = v
         queue_redraw()
         
+func _ready():
+    var images = [
+        preload("res://PoisonDrop/Sprites/banana.png"),
+        preload("res://PoisonDrop/Sprites/crumpled-bag.png"),
+        preload("res://PoisonDrop/Sprites/sodacan.png"),
+    ]
+    var sprite = Sprite2D.new()
+    sprite.texture = images.pick_random()
+    sprite.z_index = 5
+    add_child(sprite)
 
 func _draw():
     draw_circle(Vector2(), radius, Color(.4,.4,.4))
