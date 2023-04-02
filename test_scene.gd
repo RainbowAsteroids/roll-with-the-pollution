8881b723c49fafdb601883cd2bdf104af24b58c5
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-04-02 00:16:16 -0400

    fix #6

--- test_scene.gd
+++ test_scene.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+func _ready():
+    var map: MapGenerator = $PatternContainer
+    var section := map.sections[0]
+    var center := section.bounds.position + (section.bounds.size / 2)
+    var pos := map.map_to_local(center)
+    $Player.position = pos
+    print(section.bounds.position)
+    print(center)
+    print(pos)
+
 func add_poison_drop(pd: Node):
     if pd:
         $CanvasGroup.add_child(pd)
