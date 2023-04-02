60d771c017520dd34a2f7bce4fd2df6a8c1c5255
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-04-02 00:08:30 -0400

    Added poison drops; fix #3

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,20 +8,27 @@ signal dead
 @export var max_speed := 6000.0
 @export var braking_force := 2.5
 @export var poison_drop: PackedScene
+@export var poison_damage_rate := 10.0
+@export var heal_rate := 20.0
 
 var _health := GlobalConstants.max_health
 @export var health: float:
     get: return _health
     set(v):
+        v = clamp(v,0,GlobalConstants.max_health)
         _health = v
         $CanvasLayer/HealthBar.health = v
         if v < 0: 
             dead.emit()
 
+var can_heal := false
+var can_hurt := false
+
 func _draw():
     draw_circle(Vector2(), 36, Color.BLUE_VIOLET)
 
 func _physics_process(delta: float):
+    # movement
     var motion := Vector2()
     if Input.is_action_pressed("move_left"):
         motion.x -= 1
@@ -45,8 +52,32 @@ func _physics_process(delta: float):
     velocity -= velocity * friction * delta
     if velocity.length() < 1:
         velocity = Vector2()
+    
+    # check for overlapping areas
+    if $Area2D.has_overlapping_areas():
+        $HealCoolup.stop()
+        can_heal = false
+        if can_hurt:
+            health -= poison_damage_rate * delta
+        else:
+            if $DamageCoolup.is_stopped():
+                $DamageCoolup.start()
+    else:
+        $DamageCoolup.stop()
+        can_hurt = false
+        if can_heal:
+            health += heal_rate * delta
+        else:
+            if $HealCoolup.is_stopped():
+                $HealCoolup.start()
 
 func _on_poison_timer_timeout():
     var pd = poison_drop.instantiate()
     pd.position = position
     get_parent().add_poison_drop(pd)
+
+func _on_heal_coolup_timeout():
+    can_heal = true
+
+func _on_damage_coolup_timeout():
+    can_hurt = true
