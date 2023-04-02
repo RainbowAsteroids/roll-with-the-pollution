24bf96040c80718853d30b8ebfa31e349cf3501c
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-04-02 07:49:05 -0400

    Made collectables more visible

--- Collectable/Collectable.gd
+++ Collectable/Collectable.gd
@@ -1,7 +1,13 @@
 extends Area2D
 class_name Collectable
 
-var collected = false
+const initial_alpha := 0.5
+
+var collected := false
+var alpha := initial_alpha
+
+func _draw():
+    draw_circle(Vector2(), 38, Color(0.9,0.9,0.1,alpha))
 
 func _ready():
     var images := [
@@ -14,9 +20,17 @@ func _ready():
     sprite.texture = images.pick_random()
     sprite.rotation_degrees = 360 * randf()
     add_child(sprite)
+    
+    var tween = create_tween()
+    tween.tween_property(self, "alpha", 1, 1)
+    tween.tween_property(self, "alpha", initial_alpha, 1)
+    tween.set_loops()
 
 func _on_body_entered(player: Player):
     if not collected and player:
         player.collected_collectable(self)
         collected = true
         visible = false
+
+func _process(delta):
+    queue_redraw()
